163d1be1a732ae089bb6293c54513f0ec663617c
MavlonTurgunof <mavlondev2023@gmail.com>
2024-10-02 17:22:58 +0500

    first commit

---
+++ src/Pages/stats/Stats.jsx
@@ -0,0 +1,33 @@
+import VoucherCard from "@/components/card/VoucherCard";
+import { Context } from "@/context/UserContext";
+import { useFetchData } from "@/hooks";
+import { useContext } from "react";
+
+const Stats = () => {
+  const userId = window?.Telegram?.WebApp?.initDataUnsafe?.user?.id;
+
+  const { data: voucher } = useFetchData("vouchers", userId);
+
+  return (
+    <div className="stats">
+      <div className="coinCount">
+        <div className="flex items-center flex-col font-jetBrainsMono">
+          <h1 className="text-[#8C8787] text-[20px]">Total Voucher</h1>
+          <h1 className="text-center text-white text-[32px]">
+            {voucher?.voucher_count}
+          </h1>
+        </div>
+
+        <div className="w-full py-[0] px-[24px]">
+          <div className="border-b-[2px] border-[#303b58] my-[21px]"> </div>
+
+          {voucher?.vouchers?.map((voucher) => (
+            <VoucherCard key={voucher.id} {...voucher} userId={userId} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stats;
